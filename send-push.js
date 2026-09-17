e1d8de2a93b94c8a042ab0e50bee8641a218eaa4
Pedro Vinícius <vinicius.chagas@escolar.ifrn.edu.br>
2026-09-17 01:12:35 -0300

    Teste final de notificacao

--- send-push.js
+++ send-push.js
@@ -21,7 +21,7 @@ async function enviarNotificacoes() {
   const snapshot = await db.collection('push_subscriptions').get();
   
   const mensagem = JSON.stringify({
-    title: 'PicanhaFlix Atualizado!🍿',
+    title: 'PicanhaFlix Atualizado! v1.2.1 saiu!!🍿',
     body: 'Tem novidade no site! Confira as o Log de atualizações para conferir o que mudou.',
     icon: 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
   });
